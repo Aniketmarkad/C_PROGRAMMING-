f6abb31aeffd11feb2f4c7215c617d28f8f9cd3e
Aniket Markad <aniket45.markad@gmail.com>
2025-08-17 00:42:38 +0530

    Add files via upload

---
+++ Problems _On_Patterns Printing/program67.c
@@ -0,0 +1,41 @@
+/*
+    a   a   a   a
+    b   b   b   b
+    c   c   c   c
+    d   d   d   d
+*/
+
+#include<stdio.h>
+
+void Display(int iRow,int iCol)
+{
+    int i = 0;
+    int j = 0;
+    char ch = '\0';
+
+    for(i =1,ch ='a'; i <= iRow ; i++,ch++)
+    {
+        for(j = 1; j <= iCol; j++) 
+        {
+            printf(" %c",ch);
+        
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf(" Enter the no of row :\n");
+    scanf("%d",&iValue1);
+
+    printf(" Enter the no of row :\n");
+    scanf("%d",&iValue2);
+    
+    Display(iValue1,iValue2);
+
+    return 0 ;
+}
